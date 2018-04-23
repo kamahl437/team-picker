f6e99a089071ff6742a36383380dec4e5ded0d1b
JQ <kamahl437@gmail.com>
2018-04-23 19:17:45 -0400

    removing dup command

--- updateAndRun.sh
+++ updateAndRun.sh
@@ -1,5 +1,5 @@
 git pull;
 #command for cleaning up old images
 #docker images | grep "2 months" | awk '{print $3}' | xargs docker rmi -f
-docker build .;
-docker-compose up;
+#docker build .;
+docker-compose up --build 
